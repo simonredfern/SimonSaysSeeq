13e0566c9c699eabeed37e125b2e54c5142a5c12
simonredfern <simon@tesobe.com>
2025-02-11 09:53:20 +0100

    clean up 1

--- SimonSaysSeeqNorns/SimonSaysSeeqNorns.lua
+++ SimonSaysSeeqNorns/SimonSaysSeeqNorns.lua
@@ -580,7 +580,7 @@ if not (tonumber(velocity) >= 0 and tonumber(velocity) <= 127) then
   error("SendMidiKeyboardNoteOn says velocity is out of bounds with the value: " .. tostring(velocity))
 end 
 
-SanityCheckMidiChannel(channel, "SendMidiKeyboardNoteOn A")
+SanityCheckMidiChannel(channel) -- don't need to return this, just check it and carry on.
 
   print("SendMidiKeyboardNoteOn note: " .. tostring(note) .. " velocity: " .. tostring(velocity) .. " channel: " .. tostring(channel))
 
@@ -892,17 +892,17 @@ function init_flutter_window()
   flutter_tempo_sum = 0
 end  
 
-function SanityCheckMidiChannel(channel, caller)
+function SanityCheckMidiChannel(channel)
   last_function = 987643
-  print ("Hello from SanityCheckMidiChannel caller is: " .. caller)
+  print ("Hello from SanityCheckMidiChannel")
 
 
   if (channel == nil) then
-    error("SCMC channel nil caller:" .. caller) 
+    error("SanityCheckMidiChannel says channel is nil") 
   end
 
   if not (tonumber(channel) >= 0 and tonumber(channel) <= 16) then
-    error("SanityCheckMidiChannel says channel is out of bounds with the value: " .. tostring(channel) .. " caller is: " .. caller)
+    error("SanityCheckMidiChannel says channel is out of bounds with the value: " .. tostring(channel))
   end 
 
   return tonumber(channel)
@@ -942,7 +942,7 @@ function PlayMidi()
 
               -- Send MIDI Note ON
 
-              SendMidiKeyboardNoteOn(n, note_on_event.velocity, SanityCheckMidiChannel(MIDI_KEYBOARD_CHANNEL, "PlayMidi A"), "PlayMidi A2")
+              SendMidiKeyboardNoteOn(n, note_on_event.velocity, SanityCheckMidiChannel(MIDI_KEYBOARD_CHANNEL), "PlayMidi A2")
 
               -- midi_keyboard_usb_device_port:note_on (MIDI_KEYBOARD_CHANNEL, n, keyboard_midi_note_events[current_midi_lane][midi_bar_count][midi_step_count][n][1].velocity)
               -- ConditionalWriteMidiNoteOn(MIDI_KEYBOARD_CHANNEL, n, note_on_event.velocity)
@@ -973,7 +973,7 @@ function PlayMidi()
 
 
               print ("MIDI_KEYBOARD_CHANNEL is " .. MIDI_KEYBOARD_CHANNEL)
-              SendMidiKeyboardNoteOn(n, 0, SanityCheckMidiChannel(MIDI_KEYBOARD_CHANNEL, "PlayMidi B"), "PlayMidi B 2")
+              SendMidiKeyboardNoteOn(n, 0, SanityCheckMidiChannel(MIDI_KEYBOARD_CHANNEL), "PlayMidi B 2")
 
               -- midi_keyboard_usb_device_port:note_on (MIDI_KEYBOARD_CHANNEL, n, 0)
 
